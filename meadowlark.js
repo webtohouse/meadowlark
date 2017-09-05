c16d7f878453d2a1ef57eb2793fd8da6e92621c9
webtohouse <webtohouse@gmail.com>
2017-09-05 20:26:14 +0900

    동적페이지 구현

--- meadowlark.js
+++ meadowlark.js
@@ -13,6 +13,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+// 동적 콘텐츠 뷰를 위한 배열
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible keep it simple"
+]
 // Add Route
 app.get('/', function(req, res){
 		// res.type('text/plain');
@@ -23,7 +31,9 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res){
 		// res.type('text/plain');
 		// res.send('About Meadowlark Travel');
-		res.render('about');
+		var randomFortune =
+			fortunes[Math.floor(Math.random() * fortunes.length)];
+		res.render('about', { fortune:randomFortune });
 });
 
 // 커스텀 404 페이지
