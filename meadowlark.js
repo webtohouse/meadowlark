ec08f66f8558c3d4f372280959b5662f0236e7e2
webtohouse <webtohouse@gmail.com>
2017-09-06 00:25:49 +0900

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,4 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 var app = express();
 
@@ -13,14 +14,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
-// 동적 콘텐츠 뷰를 위한 배열
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible keep it simple"
-]
+
 // Add Route
 app.get('/', function(req, res){
 		// res.type('text/plain');
@@ -31,9 +25,7 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res){
 		// res.type('text/plain');
 		// res.send('About Meadowlark Travel');
-		var randomFortune =
-			fortunes[Math.floor(Math.random() * fortunes.length)];
-		res.render('about', { fortune:randomFortune });
+		res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 커스텀 404 페이지
