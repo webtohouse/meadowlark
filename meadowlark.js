3cb34efcd59160b545d4e3450a5739410fbf0a04
webtohouse <webtohouse@gmail.com>
2017-09-05 18:46:43 +0900

    Add Route

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,17 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+// Add Route
+app.get('/', function(req, res){
+		res.type('text/plain');
+		res.send('Meadowlark Travel');
+});
+
+app.get('/about', function(req, res){
+		res.type('text/plain');
+		res.send('About Meadowlark Travel');
+});
+
 // 커스텀 404 페이지
 app.use(function(req, res){
 	res.type('text/plain');
